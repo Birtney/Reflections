910f8596f8821baf9178aa3514f5d7263042593b
Komoribe <xvelpro8@gmail.com>
2024-11-10 00:13:45 +0800

    feat: init.

--- components/Player.js
+++ components/Player.js
@@ -74,7 +74,7 @@ const Player = () => {
           lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
           api={siteConfig(
             'MUSIC_PLAYER_METING_API',
-            'https://api.injahow.cn/meting'
+            'https://api.injahow.cn/meting/?server=tencent&type=playlist&id=2745862858'
           )}
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
